1a3fbb582ab55ebf21cc007382ea7dc881cb2d88
ymg24 <021200643i@uandina.edu.pe>
2025-05-28 23:23:02 -0500

    a

--- estructura/header.php
+++ estructura/header.php
@@ -1,5 +1,6 @@
 <?php
-// Iniciar sesión si no está iniciada
+// Iniciar sesión si no está iniciad
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }require_once __DIR__ . '/../conexion/config.php'; // Ajusta según tu estructura
